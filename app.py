ef8450daf207b38f3b4640fef492860467b6dc23
Birat Poudel <poudel.birat25@gmail.com>
2024-09-10 23:35:38 +0545

    [Refactor]: Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def get_vector_store(documents):
         return building_vector_store(documents)
 
 template = """Always Greet the user. Use the following pieces of context to answer the question at the end.
+Try to give meaningful answers to all the questions related to Jobsflow.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 Use three sentences maximum and keep the answer as concise as possible.
 
